23dd788e2daa269aec47a48061604a908225c5ce
puutaro <hoperstep@gmail.com>
2023-01-19 19:26:44 +0900

    fd sort but fix

--- linux/srcs/difbk_dir/difbk_lib/exec_difbk_bk_lib/get_buckup_con_from_recent_merge_con_lib/exec_get_buckup_con_from_recent_merge_con.sh
+++ linux/srcs/difbk_dir/difbk_lib/exec_difbk_bk_lib/get_buckup_con_from_recent_merge_con_lib/exec_get_buckup_con_from_recent_merge_con.sh
@@ -5,7 +5,6 @@ exec_get_buckup_con_from_recent_merge_con(){
 	local merge_list_file_path="$(\
 		echo_merge_list_file_path \
 	)"
-
 	case "${merge_list_file_path}" in 
 		"") echo ""
 			return
@@ -22,6 +21,7 @@ echo_merge_list_file_path(){
 	if [ -e "${BUCK_UP_DIR_PATH}" ];then
 		fd . "${BUCK_UP_DIR_PATH}" -d 6 \
 			| rga "/${BUCK_UP_DIR_NAME}/[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{4}/${BUCKUP_MERGE_CONTENSTS_LIST_DIR_NAME}/${BUCKUP_MERGE_CONTENSTS_LIST_FILE_NAME}" \
+			| sort \
 			| tail -n 1 \
 		2>/dev/null
 		return
