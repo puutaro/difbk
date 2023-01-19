266986f6f2d84100ee955c31acc36c682fcbd2ff
puutaro <hoperstep@gmail.com>
2023-01-19 19:50:44 +0900

    fd no sort bug fix

--- linux/srcs/difbk_dir/difbk_lib/exec_dfbk_reset_lib/echo_recent_backup_datetime_directory_path.sh
+++ linux/srcs/difbk_dir/difbk_lib/exec_dfbk_reset_lib/echo_recent_backup_datetime_directory_path.sh
@@ -7,6 +7,7 @@ echo_recent_backup_datetime_directory_path(){
 		-t d \
 		"${BUCK_UP_DIR_PATH}" -d ${merge_list_depth} \
 	| rga "/${BUCK_UP_DIR_NAME}/[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{4}" \
+	| sort \
 	| tail -n 1 \
 	| rga -o "/${BUCK_UP_DIR_NAME}/[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{4}"
 }
--- linux/srcs/difbk_dir/difbk_lib/exec_difbk_diff_lib/echo_recent_merge_list_path.sh
+++ linux/srcs/difbk_dir/difbk_lib/exec_difbk_diff_lib/echo_recent_merge_list_path.sh
@@ -6,5 +6,6 @@ echo_recent_merge_list_path(){
 	fd . \
 		"${BUCK_UP_DIR_PATH}" -d ${merge_list_depth} \
 	| rga "/${BUCK_UP_DIR_NAME}/[0-9]{4}/[0-9]{2}/[0-9]{2}/[0-9]{4}/${BUCKUP_MERGE_CONTENSTS_LIST_DIR_NAME}/${BUCKUP_MERGE_CONTENSTS_LIST_FILE_NAME}" \
+	| sort  \
 	| tail -n 1
 }
