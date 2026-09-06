b3861068380c269f2e00ffe5826ec8b8cf38feee
puutaro <hoperstep@gmail.com>
2026-09-06 20:19:17 +0900

    reback backpu proceess fd ignore

--- linux/srcs/difbk_dir/difbk_lib/exec_difbk_bk_lib/checksum_calc_and_write_out_to_file.sh
+++ linux/srcs/difbk_dir/difbk_lib/exec_difbk_bk_lib/checksum_calc_and_write_out_to_file.sh
@@ -4,26 +4,23 @@
 checksum_calc_and_write_out_to_file(){
 	local difbk_sum_dummy_file_name="check_sum_dummy_file"
 	local dfbk_exec_sum_file_path="${DFBK_SETTING_DIR_PATH}/exec_sum.sh"
-		# 1. 除外パターンのリストを配列に読み込む
-    # (sedのオプションを正しい -E に修正)
-    readarray -t ignore_args < <(
-        cat "${DFBK_IGNORE_FILE_PATH}" \
-            | rga -v "^#" \
-            | sed \
-                -e 's/\[/\\\[/g' \
-                -e 's/\]/\\\]/g' \
-                -E -e 's/^([^ ])/\/\1/' \
-                -E -e 's/([^ ])$/\1/' \
-                -e 's/\/\//\//' \
-                -E -e 's/^([^ ])/ -E \1/'
-    )
-
-    # 2. fd コマンドを配列として組み立てて実行する
-    # 配列を展開することで、引数の空白や特殊文字が安全に維持されます
-    fd \
-      -IH "${ignore_args[@]}" \
-      . \
-      "${TARGET_DIR_PATH}" \
+	eval "\
+		fd -IH \
+			$(\
+				cat "${DFBK_IGNORE_FILE_PATH}" \
+					| rga -v "^#" \
+					| sed \
+						-e 's/\[/\\\[/g' \
+						-e 's/\]/\\\]/g' \
+						-re "s/^([^ ])/\/\1/" \
+						-re "s/([^ ])$/\1/" \
+						-e 's/\/\//\//' \
+						-re 's/^([^ ])/ -E\ \1/' \
+					| tr -d '\n'\
+			) \
+			. \
+			\"${TARGET_DIR_PATH}\"\
+		" \
 		| rga -v "/${DFBK_UTIL_DIR_NAME}" \
 		| sed "1i ${TARGET_DIR_PATH}" \
 		| rga -v "${DFBK_IGNORE_FILE_NAME}$" \
