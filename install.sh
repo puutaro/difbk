6dbc02a91e692577f1bb28520c08fa2b8fe664f9
kitamura.takeyuki <kitamura.takeyuki@tecotec.co.jp>
2026-09-07 22:24:42 +0900

    modiy

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo -v
 readonly OS_TYPE="$(uname -s)"
 
 if [ "${OS_TYPE}" = "Darwin" ]; then
-  curl -fsSL "https://raw.githubusercontent.com/puutaro/cmdclick2/refs/heads/master/install/mac/installer.sh" | bash
+  curl -fsSL "https://raw.githubusercontent.com/puutaro/difbk/refs/heads/master/install/mac/installer.sh" | bash
 else
   curl -fsSL "https://raw.githubusercontent.com/puutaro/difbk/refs/heads/master/install/linux/installer.sh" | bash
 fi
